64a5e8ac7b1b18886e99431483fd7b9600331afb
iGotYourBackMr <erikssonanton2001@gmail.com>
2024-12-19 01:48:02 +0100

    Adapt for FPS.ms fixed startup command

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Current directory: $(pwd)"
-echo "Listing files:"
-ls -la
-echo "Installing requirements..."
-pip install -r requirements.txt
-echo "Starting bot..."
-python3 app.py 
